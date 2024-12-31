59d85352957bde3f7535999a1c1a9e200d29713f
gaur8126 <gaurlokesh1211@gmail.com>
2024-12-31 19:26:01 +0530

    utils file added

--- networksecurity/components/data_validation.py
+++ networksecurity/components/data_validation.py
@@ -11,9 +11,14 @@ import os,sys
 
 
 class DataValidation:
-    def __init__(self):
-        pass
-
+    def __init__(self,data_ingestion_artifact:DataIngestionArtifact,
+                    data_validation_config:DataValidationConfig):
+        try:
+            self.data_ingestion_artifact = data_ingestion_artifact
+            self.data_validation_config = data_validation_config
+            self._schema_config = read_yaml_file_path(SCHEMA_FILE_PATH)
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
 
     def validate_number_of_columns(self,dataframe:pd.DataFrame)->bool:
         try:
