a4367ac40be5040ba586a355620d9bac90030b14
gaur8126 <gaurlokesh1211@gmail.com>
2025-01-02 15:25:44 +0530

    mode trainer is ready to code

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -4,7 +4,12 @@ import sys
 from networksecurity.logger.logger import logging 
 from networksecurity.exception.exception import NetworkSecurityException
 
+from networksecurity.entity.artifact_entity import DataTransformationArtifact,ModelTrainerArtifact
+from networksecurity.entity.config_entity import ModelTrainerConfig
 
+from xgboost import XGBClassifier
+from networksecurity.utils.ml_utils.model.estimator import NetworkModel
+from networksecurity.utils.main_utils.utils import load_numpy_array_data,save_object,load_object
 
 
 class ModelTrainer:
