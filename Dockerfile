715358c980fbe1d0ec792c9b2b85956e3cbbb620
gaur8126 <gaurlokesh1211@gmail.com>
2025-01-06 23:46:14 +0530

    updated the  workflow

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,18 @@
 FROM python:3.10-slim-buster
-WORKDIR /app
-COPY . /app
-
+USER root
+RUN mkdir /app
+COPY . /app/
+WORKDIR /app/
+RUN pip3 install -r requirements.txt
+ENV AWS_DEFAULT_REGION = "ap-south-1"
+ENV BUCKET_NAME="network.security"
+ENV PREDICTION_BUCKET_NAME="Network-dataSource"
+ENV AIRFLOW_HOME="/app/airflow"
+ENV AIRFLOW_CORE_DAGBAG_IMPORT_TIMEOUT=1000
+ENV AIRFLOW_CORE_ENABLE_XCOM_PICKLING=True
+RUN airflow db init
+RUN airflow users create -e gaurlokesh1211@gmail.com -f lokesh -l gaur -p admin -r Admin -u admin
+RUN chmod 777 start.sh
 RUN apt update -y
-
-RUN apt-get update && pip install -r requirements.txt
-CMD ["python3","app.py"]
+ENTRYPOINT [ "/bin/sh" ]
+CMD ["start.sh"]
--- networksecurity/pipeline/training_pipeline.py
+++ networksecurity/pipeline/training_pipeline.py
@@ -33,11 +33,16 @@ from networksecurity.entity.artifact_entity import (
     ModelPusherArtifact
 )
 
+from networksecurity.cloud.s3syncer import S3Sync
+from networksecurity.constant.training_pipeline import TRAINING_BUCKET_NAME
+from networksecurity.constant.training_pipeline import SAVE_MODEL_DIR
 
 
 class TrainingPipeline:
+    is_pipeline_running = False
     def __init__(self):
         self.training_pipeline_config = TrainingPipelineConfig()
+        self.s3_sync = S3Sync()
 
     def start_data_ingestion(self):
         try:
@@ -109,6 +114,20 @@ class TrainingPipeline:
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
+    def sync_artifact_dir_to_s3(self):
+        try:
+            aws_bucket_url = f"s3://{TRAINING_BUCKET_NAME}/artifact/{self.training_pipeline_config.timestamp}"
+            self.s3_sync.sync_folder_to_s3(folder = self.training_pipeline_config.artifact_dir,aws_bucket_url=aws_bucket_url)
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
+    def sync_saved_model_dir_to_s3(self):
+        try:
+            aws_bucket_url = f"s3://{TRAINING_BUCKET_NAME}/{SAVE_MODEL_DIR}"
+            self.s3_sync.sync_folder_to_s3(folder=SAVE_MODEL_DIR,aws_bucket_url=aws_bucket_url)
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
 
     def run_pipeline(self):
         try:
@@ -125,7 +144,13 @@ class TrainingPipeline:
             
             model_pusher_artifact = self.start_model_pusher(model_eval_artifact)
 
+            TrainingPipeline.is_pipeline_running = False
+            self.sync_artifact_dir_to_s3()
+            self.sync_saved_model_dir_to_s3()
+
         except Exception as e:
+            self.sync_artifact_dir_to_s3()
+            TrainingPipeline.is_pipeline_running = False
             raise NetworkSecurityException(e,sys)
         
     
